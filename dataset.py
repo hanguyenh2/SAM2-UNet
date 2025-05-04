6d008add3f1bdcc0c1ec00ad2a1e91972a9d2924
hanguyenh2 <ha.nguyen@h2corporation.jp>
2025-05-04 20:40:37 +0700

    revert dataset.py

--- dataset.py
+++ dataset.py
@@ -2,26 +2,67 @@ import os
 import random
 
 import numpy as np
+import torchvision.transforms.functional as F
 from PIL import Image
 from torch.utils.data import Dataset
 from torchvision import transforms
+from torchvision.transforms import InterpolationMode
 
 
-# Define a transform that randomly rotates the image by 90, 180, or 270 degrees
-class RandomRotation90180270(transforms.RandomRotation):
+class ToTensor(object):
+
+    def __call__(self, data):
+        image, label = data['image'], data['label']
+        return {'image': F.to_tensor(image), 'label': F.to_tensor(label)}
+
+
+class Resize(object):
+
+    def __init__(self, size):
+        self.size = size
+
+    def __call__(self, data):
+        image, label = data['image'], data['label']
+
+        return {'image': F.resize(image, self.size),
+                'label': F.resize(label, self.size, interpolation=InterpolationMode.BICUBIC)}
+
+
+class RandomHorizontalFlip(object):
     def __init__(self, p=0.5):
-        super().__init__(degrees=[0, 0])  # Initialize with 0 degrees
         self.p = p
 
-    def get_params(self, img):
+    def __call__(self, data):
+        image, label = data['image'], data['label']
+
         if random.random() < self.p:
-            angle = random.choice([90, 180, 270])
-        else:
-            angle = 0
-        return angle
+            return {'image': F.hflip(image), 'label': F.hflip(label)}
+
+        return {'image': image, 'label': label}
 
 
-random_rotation = RandomRotation90180270(p=0.5)
+class RandomVerticalFlip(object):
+    def __init__(self, p=0.5):
+        self.p = p
+
+    def __call__(self, data):
+        image, label = data['image'], data['label']
+
+        if random.random() < self.p:
+            return {'image': F.vflip(image), 'label': F.vflip(label)}
+
+        return {'image': image, 'label': label}
+
+
+class Normalize(object):
+    def __init__(self, mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]):
+        self.mean = mean
+        self.std = std
+
+    def __call__(self, sample):
+        image, label = sample['image'], sample['label']
+        image = F.normalize(image, self.mean, self.std)
+        return {'image': image, 'label': label}
 
 
 class FullDataset(Dataset):
@@ -33,18 +74,17 @@ class FullDataset(Dataset):
         self.gts = sorted(self.gts)
         if mode == 'train':
             self.transform = transforms.Compose([
-                transforms.Resize((size, size)),
-                random_rotation,
-                transforms.ToTensor(),
-                transforms.Normalize([0.485, 0.456, 0.406],
-                                     [0.229, 0.224, 0.225])
+                Resize((size, size)),
+                RandomHorizontalFlip(p=0.5),
+                RandomVerticalFlip(p=0.5),
+                ToTensor(),
+                Normalize()
             ])
         else:
             self.transform = transforms.Compose([
-                transforms.Resize((size, size)),
-                transforms.ToTensor(),
-                transforms.Normalize([0.485, 0.456, 0.406],
-                                     [0.229, 0.224, 0.225])
+                Resize((size, size)),
+                ToTensor(),
+                Normalize()
             ])
 
     def __getitem__(self, idx):
