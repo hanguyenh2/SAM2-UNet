9bbd6592041cbad387bbdb719fee635b71fe5c5f
hanguyenh2 <ha.nguyen@h2corporation.jp>
2025-05-06 12:46:25 +0700

    Add time test

--- test.py
+++ test.py
@@ -1,17 +1,19 @@
 import argparse
 import os
-import torch
+import time
+
 import imageio
 import numpy as np
+import torch
 import torch.nn.functional as F
+
 from SAM2UNet import SAM2UNet
 from dataset import TestDataset
 
-
 parser = argparse.ArgumentParser()
 parser.add_argument("--checkpoint", type=str, required=True,
-                help="path to the checkpoint of sam2-unet")
-parser.add_argument("--test_image_path", type=str, required=True, 
+                    help="path to the checkpoint of sam2-unet")
+parser.add_argument("--test_image_path", type=str, required=True,
                     help="path to the image files for testing")
 parser.add_argument("--test_gt_path", type=str, required=True,
                     help="path to the mask files for testing")
@@ -20,7 +22,6 @@ parser.add_argument("--save_path", type=str, required=True,
 parser.add_argument("--size", default=1024, type=int)
 args = parser.parse_args()
 
-
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 test_loader = TestDataset(args.test_image_path, args.test_gt_path, args.size)
 model = SAM2UNet().to(device)
@@ -28,12 +29,16 @@ model.load_state_dict(torch.load(args.checkpoint), strict=True)
 model.eval()
 model.cuda()
 os.makedirs(args.save_path, exist_ok=True)
+test_time = []
 for i in range(test_loader.size):
     with torch.no_grad():
         image, gt, name = test_loader.load_data()
         gt = np.asarray(gt, np.float32)
         image = image.to(device)
+        time_start = time.time()
         res, _, _ = model(image)
+        process_time = time.time() - time_start
+        test_time.append(process_time)
         # fix: duplicate sigmoid
         # res = torch.sigmoid(res)
         res = F.upsample(res, size=gt.shape, mode='bilinear', align_corners=False)
@@ -47,4 +52,7 @@ for i in range(test_loader.size):
         # res[res >= int(255 * lambda)] = 255
         # res[res < int(255 * lambda)] = 0
         print("Saving " + name)
+        print("process_time:", process_time)
         imageio.imsave(os.path.join(args.save_path, name[:-4] + ".png"), res)
+
+print("test_time:", np.mean(test_time))
