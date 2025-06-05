45b74c918796f74cc47344c4d801915e6492ff62
hanguyenh2 <ha.nguyen@h2corporation.jp>
2025-06-06 02:31:40 +0700

    Remove Adapter, train size 768

--- test.py
+++ test.py
@@ -22,7 +22,7 @@ parser.add_argument("--test_image_path", type=str,
 parser.add_argument("--test_gt_path", type=str,
                     default="../data_crop/data_test/masks/",
                     help="path to the mask files for testing")
-parser.add_argument("--size", default=1280, type=int)
+parser.add_argument("--size", default=768, type=int)
 args = parser.parse_args()
 
 # 2. Set device to cuda
