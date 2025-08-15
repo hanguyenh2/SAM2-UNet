647e069ab275b260de221666654e35b641b2a4f2
hanguyenh2 <ha.nguyen@h2corporation.jp>
2025-08-15 08:17:13 +0700

    add weight to boundary

--- train.py
+++ train.py
@@ -41,7 +41,7 @@ args = parser.parse_args()
 
 
 def structure_loss(pred, mask):
-    weit = 1 + 5 * torch.abs(F.avg_pool2d(mask, kernel_size=31, stride=1, padding=15) - mask)
+    weit = 1 + 10 * torch.abs(F.avg_pool2d(mask, kernel_size=31, stride=1, padding=15) - mask)
     wbce = F.binary_cross_entropy_with_logits(pred, mask, reduce='none')
     wbce = (weit * wbce).sum(dim=(2, 3)) / weit.sum(dim=(2, 3))
     pred = torch.sigmoid(pred)
